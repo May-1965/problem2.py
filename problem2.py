47508f44f964b4f160dc5a106e5c7833a09d110e
Maysam AlKhusaibi <149917210+May-1965@users.noreply.github.com>
2024-07-21 07:48:33 +0400

    Update and rename me.py to problem2.py

---
+++ problem2.py
@@ -0,0 +1,2 @@
+import streamlit as st
+print("Cyber security refers to every aspect of protecting an organization and its employees and assets against cyber threats. As cyberattacks become more common and sophisticated and corporate networks grow more complex, a variety of cyber security solutions are required to mitigate corporate cyber risk.")
